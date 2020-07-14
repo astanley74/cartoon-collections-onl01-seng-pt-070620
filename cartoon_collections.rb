4d41780c89c8125dfb1b715db9022ffd90ce5ea1
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-14 18:30:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
